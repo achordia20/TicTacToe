baa1ce8cb33f307aa141cc64cf8630241d6173e7
Abhinav Chordia <achordia20@gmail.com>
2014-02-07 02:03:19 -0600

    refactor calculateScore

--- src/TicTacToeAI.java
+++ src/TicTacToeAI.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class TicTacToeAI {
@@ -66,64 +67,91 @@ public class TicTacToeAI {
 	
 	private int getBoardScore() {
 		int score = 0;
-		score += calculateScore(1, 2, 3);
-		score += calculateScore(4, 5, 6);
-		score += calculateScore(7, 8, 9);
-		score += calculateScore(1, 4, 7);
-		score += calculateScore(2, 5, 8);
-		score += calculateScore(3, 6, 9);
-		score += calculateScore(1, 5, 9);
-		score += calculateScore(3, 5, 7);
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(1, 2, 3)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(4, 5, 6)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(7, 8, 9)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(1, 4, 7)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(2, 5, 8)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(3, 6, 9)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(1, 5, 9)));
+		score += calculateScore(new ArrayList<Integer>(Arrays.asList(3, 5, 7)));
 		return score;
 	}
 	
-	private int calculateScore(int pos1, int pos2, int pos3) {
-		MoveType m1 = board.atPos(pos1);
-		MoveType m2 = board.atPos(pos2);
-		MoveType m3 = board.atPos(pos3);
-		
-		if (m1 == myMoveType) {
-			return getScoreForRow(myMoveType, m1, m2, m3);
-		} else if (m1 == theirMoveType) {
-			return getScoreForRow(theirMoveType, m1, m2, m3);
-		} else {
-			return calculateFromEmptyMoveType(m1, m2, m3);
+	private int calculateScore(ArrayList<Integer> positions) {
+		int myMoveTypeCount = 0, theirMoveCount = 0, emptyMoveCount = 0;
+		for (Integer pos : positions) {
+			MoveType m = board.atPos(pos);
+			if (myMoveType == m)
+				myMoveTypeCount++;
+			else if (theirMoveType == m)
+				theirMoveCount++;
+			else
+				emptyMoveCount++;
 		}
-	}
-	
-	private int getScoreForRow(MoveType base, MoveType m1, MoveType m2, MoveType m3) {
-		if (base.other() == m2 || base.other() == m3)
+		
+		if (myMoveTypeCount > 0 && theirMoveCount > 0)
 			return 0;
 		
-		if (m2 == base && m3 == base)
-			return (base == myMoveType) ? 100 : -100;
-		else if (m2 == base || m3 == base)
-			return (base == myMoveType) ? 10 : -10;
-		else
-			return (base == myMoveType) ? 1 : -1;
+		if (emptyMoveCount == positions.size())
+			return 0;
+		
+		if (myMoveTypeCount > 0)
+			return (int) Math.pow(10, myMoveTypeCount);
+		
+		if (theirMoveCount > 0)
+			return -1 * (int)Math.pow(10, theirMoveCount);
+		
+		return 0;
 	}
 	
-	private int calculateFromEmptyMoveType(MoveType m1, MoveType m2, MoveType m3) {
-		if (m2 == myMoveType) {
-			if (m3 == theirMoveType)	// both moveTypes there
-				return 0;
-			
-			if (m3 == myMoveType)	// 2/ 3 same as AI
-				return 10;
-			return 1;	// only one same as AI
-		} else if (m2 == theirMoveType) {
-			if (m3 == myMoveType)			// both moveTypes there
-				return 0;
-			
-			if (m3 == theirMoveType)	// 2/3 same as user
-				return -10;
-			return -1;		//only one same as user
-		} else {
-			if (m3 == myMoveType)	// only one same as AI
-				return 1;
-			else if (m3 == theirMoveType)	// only one same as user
-				return -1;
-			return 0;	// all blank
-		}
-	}
+//	private int calculateScore(int pos1, int pos2, int pos3) {
+//		MoveType m1 = board.atPos(pos1);
+//		MoveType m2 = board.atPos(pos2);
+//		MoveType m3 = board.atPos(pos3);
+//		
+//		if (m1 == myMoveType) {
+//			return getScoreForRow(myMoveType, m1, m2, m3);
+//		} else if (m1 == theirMoveType) {
+//			return getScoreForRow(theirMoveType, m1, m2, m3);
+//		} else {
+//			return calculateFromEmptyMoveType(m1, m2, m3);
+//		}
+//	}
+//	
+//	private int getScoreForRow(MoveType base, MoveType m1, MoveType m2, MoveType m3) {
+//		if (base.other() == m2 || base.other() == m3)
+//			return 0;
+//		
+//		if (m2 == base && m3 == base)
+//			return (base == myMoveType) ? 100 : -100;
+//		else if (m2 == base || m3 == base)
+//			return (base == myMoveType) ? 10 : -10;
+//		else
+//			return (base == myMoveType) ? 1 : -1;
+//	}
+//	
+//	private int calculateFromEmptyMoveType(MoveType m1, MoveType m2, MoveType m3) {
+//		if (m2 == myMoveType) {
+//			if (m3 == theirMoveType)	// both moveTypes there
+//				return 0;
+//			
+//			if (m3 == myMoveType)	// 2/ 3 same as AI
+//				return 10;
+//			return 1;	// only one same as AI
+//		} else if (m2 == theirMoveType) {
+//			if (m3 == myMoveType)			// both moveTypes there
+//				return 0;
+//			
+//			if (m3 == theirMoveType)	// 2/3 same as user
+//				return -10;
+//			return -1;		//only one same as user
+//		} else {
+//			if (m3 == myMoveType)	// only one same as AI
+//				return 1;
+//			else if (m3 == theirMoveType)	// only one same as user
+//				return -1;
+//			return 0;	// all blank
+//		}
+//	}
 }
